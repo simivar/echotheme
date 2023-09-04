1da3b4fc6f5d401955b9416e910cd7a916482e0a
simivar <simivar@gmail.com>
2023-09-05 01:01:44 +0200

    First views implementation

--- Templates/Archive/CategorySidebar.php
+++ Templates/Archive/CategorySidebar.php
@@ -5,28 +5,16 @@ declare(strict_types=1);
 namespace echotheme\Templates\Archive;
 
 use echotheme\Dto\CategoryWithRecentPostDto;
-use function esc_url;
 
 class CategorySidebar
 {
     /**
-     * @param CategoryWithRecentPostDto[] $data
+     * @param CategoryWithRecentPostDto[] $categories
      */
-    public static function get(array $data): string {
-        $html = '';
-        foreach ($data as $category) {
-            $html .= self::renderSingle($category);
-        }
-
-        return $html;
-    }
-
-    private static function renderSingle(CategoryWithRecentPostDto $category): string
-    {
+    public static function get(array $categories): string {
         ob_start();
         include __DIR__ . '/../../views/blocks/sidebar/category_badge.php';
-        $contents = ob_get_clean();
 
-        return $contents;
+        return ob_get_clean();
     }
 }
--- views/blocks/sidebar/category_badge.php
+++ views/blocks/sidebar/category_badge.php
@@ -1,11 +1,11 @@
 <?php
-/** @var \echotheme\Dto\CategoryWithRecentPostDto $category */
-
-$color = echotheme\Services\ArbitraryStringToHexColor::generate($category->name);
+/** @var \echotheme\Dto\CategoryWithRecentPostDto[] $categories */
+foreach ($categories as $category):
+    $color = echotheme\Services\ArbitraryStringToHexColor::generate($category->name);
 ?>
-
-<div class="rounded p-2 mb-2" style="background-color: #<?php echo $color; ?>25">
-    <a href="<?php echo esc_url(get_category_link($category->id)); ?>" class="text-decoration-none" style="color: #<?php echo $color; ?>">
-        <h6 class="m-0"><?php echo $category->name; ?></h6>
-    </a>
-</div>
+    <div class="rounded p-2 mb-2" style="background-color: #<?php echo $color; ?>25">
+        <a href="<?php echo esc_url(get_category_link($category->id)); ?>" class="text-decoration-none" style="color: #<?php echo $color; ?>">
+            <h6 class="m-0"><?php echo $category->name; ?></h6>
+        </a>
+    </div>
+<?php endforeach; ?>
